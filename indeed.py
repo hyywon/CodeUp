adfafa369c056ae03e791a73f48c5ac1c82b083d
DESKTOP-2PAA9OC\예원 <noddy0501@gmail.com>
2020-07-21 02:35:44 +0900

    WebScrapper.py
    Python으로 Web Scrapper만들기
    Web에서 각 page마다 status_code 출력해서 requests 확인하기
    Web에서 각 page마다 job 정보, company 정보 가져오기

--- indeed.py
+++ indeed.py
@@ -20,7 +20,24 @@ def extract_indeed_pages():
 
 def extract_indeed_jobs(last_page):
   jobs = []
-  for page in range(last_page) :
-    result=requests.get(f"{URL}&start={page*LIMIT}")
-    print(result.status_code)
+  # for page in range(last_page) :
+  result = requests.get(f"{URL}&start={0*LIMIT}")
+  soup = BeautifulSoup(result.text, "html.parser")
+  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})#resource 는 list
+  for result in results:
+      title = result.find("h2",{"class":"title"}).find("a")["title"]
+      company = result.find("span",{"class":"company"})
+      company_anchor = company.find("a")
+
+      if company_anchor is not None:
+        # anchor tag가 있는 경우
+        #print(company_anchor.string)
+          company = str(company_anchor.string)
+      else :
+        # anchor tag가 없는 경우
+        #print(company.string)
+          company =str(company.string)
+
+      company = company.strip()
+      print(company)
   return jobs
