12224d255ea74688c4a3d3da8e57caed5f9380be
DESKTOP-2PAA9OC\예원 <noddy0501@gmail.com>
2020-07-21 18:49:24 +0900

    WebScrapper.py
    Python으로 Web Scrapper만들기
    Indeed & Stackoverflow에서 각 page마다 job 정보, company 정보, location 정보 가져오기
    가져온 정보 csv 파일로 정리해서 저장하기

--- indeed.py
+++ indeed.py
@@ -4,40 +4,61 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
+
 #indeed page extract
-def extract_indeed_pages():
-  result = requests.get(URL)
-  soup = BeautifulSoup(result.text, "html.parser")
-  pagination = soup.find("div",{"class":"pagination"})
-  links = pagination.find_all('a')
-  pages = []
-  for link in links[:-1]:
-      pages.append(int(link.string))
-  #마지막 페이지 추출
-  max_page = pages[-1]
-  return max_page
+def get_last_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+    #마지막 페이지 추출
+    max_page = pages[-1]
+    return max_page
 
 
-def extract_indeed_jobs(last_page):
-  jobs = []
-  # for page in range(last_page) :
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text, "html.parser")
-  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})#resource 는 list
-  for result in results:
-      title = result.find("h2",{"class":"title"}).find("a")["title"]
-      company = result.find("span",{"class":"company"})
+#request 결과를 담은 html에서 job&company 추출
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    if company:
       company_anchor = company.find("a")
-
       if company_anchor is not None:
         # anchor tag가 있는 경우
-        #print(company_anchor.string)
           company = str(company_anchor.string)
-      else :
+      else:
         # anchor tag가 없는 경우
-        #print(company.string)
-          company =str(company.string)
-
+          company = str(company.string)
       company = company.strip()
-      print(company)
+    else :
+      company = None
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {
+        'title': title,
+        'company': company,
+        'location': location,
+        "link": f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
+
+
+def extract_jobs(last_page):
+  jobs = []
+  for page in range(last_page) :
+    print(f"Scrapping Indeed : page {page}")
+    result = requests.get(f"{URL}&start={page*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    #results는 list
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
+  return jobs
+
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
   return jobs
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from indeed import extract_indeed_pages,extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
+from save import save_to_file
 
-last_page = extract_indeed_pages()
-
-extract_indeed_jobs(last_page)
+so_jobs = get_so_jobs()
+indeed_jobs= get_indeed_jobs()
+jobs = indeed_jobs + so_jobs
+save_to_file(jobs)
